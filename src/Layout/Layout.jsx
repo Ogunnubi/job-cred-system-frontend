c4ce1622a86866efd92bbc95e1da4bf01d57a2db
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-11 06:43:16 -0700

    add css style

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 // import Footer from '../components/Footer/Footer'
-// import Header from '../components/Header/Header'
+import Header from '../Components/Header/Header'
 
 const Layout = ({children}) => {
   return (
     <main className='App'>
-      {children}
+        <Header />
+        {children}
     </main>
   )
 }
