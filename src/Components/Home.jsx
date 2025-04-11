d907bdf0d0c62d74e6e2c71acc966314a4f97b14
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-11 09:56:18 -0700

    add user form

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,29 +8,41 @@ const Home = () => {
             <div className="row">
                 <div className="col-lg-5">
                     <div className='intro__page'>
-                        <h3>WeWork</h3>
+                        <h1>WeWork</h1>
                         <p>Submit Jobs with Credits</p>
                     </div>
                 </div>
 
-                <div className="col-lg-6">
-                    <form action="">
+                <div className="col-lg-6 offset-lg-1">
+                    <form action="row g-3 px-3">
                         {/* name input */}
                         <div className="mb-3 input-group">
-                            <label for="nameInput" class="form-label border-start-0">Name</label>
-                            <input type="text" class="form-control" id="nameInput" />
+                            {/* <label for="nameInput" class="form-label input-group-text">Name</label> */}
+                            <input 
+                                type="email" 
+                                class="form-control border-start-0 border-end-0" 
+                                id="nameInput" 
+                                placeholder='Enter your email'
+                                required
+                            />
                         </div>
 
                         {/* email input */}
                         <div class="mb-3 input-group">
-                            <label for="emailInput" class="form-label">Email</label>
-                            <input type="email" class="form-control border-start-0 border-end-0" id="emailInput" />
+                            {/* <label for="emailInput" class="form-label input-group-text">Email</label> */}
+                            <input 
+                                type="password" 
+                                class="form-control border-start-0 border-end-0" 
+                                id="emailInput" 
+                                required
+                                placeholder='Enter your password'
+                            />
                         </div>
 
 
-                        <div className="col-12 d-flex justify-content-center">
-                            <button type="submit" className="button btn-brand">Log in</button>
-                        </div>
+                        <button type="submit" className="btn btn-brand" style={{width: "100%"}}>Log in</button>
+                        {/* <div className="col-12 d-flex justify-content-center">
+                        </div> */}
 
                     </form>
                 </div>
