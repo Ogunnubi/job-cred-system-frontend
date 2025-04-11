3f79dc12a7417cbdbf46fe5f6db314e402d7f32d
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-11 05:46:35 -0700

    add Layout

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Footer from '../components/Footer/Footer'
+// import Header from '../components/Header/Header'
+
+const Layout = ({children}) => {
+  return (
+    <main className='App'>
+      {children}
+    </main>
+  )
+}
+
+export default Layout
