52691d63b1b358c050dc0fbf186ebebecb12e0d7
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-11 06:32:17 -0700

    add more npm packages

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'remixicon/fonts/remixicon.css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import 'owl.carousel/dist/owl.carousel.js';
+import 'owl.carousel';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
