2f7327702d72f4954d44d44504e4a23218b12f7d
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-11 05:49:53 -0700

    create App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import { useState } from 'react'
 import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from '../context/AuthContext';
+import Signup from './Components/Signup';
+
+
+// Protected route component
+const ProtectedRoute = ({ children }) => {
+  const { currentUser } = useAuth();
+  
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+  
+  return children;
+};
 
 function App() {
+
+
   const [count, setCount] = useState(0)
 
+
+
+
+
+
+
+  
   return (
     <>
       
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { auth } from '../firebase/config.js';
 import { 
